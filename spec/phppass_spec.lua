77b909dd682e24fba91baeb170e453b4c1943ee4
Boris Nagaev <bnagaev@gmail.com>
2015-05-13 02:20:23 +0300

    implement checking a password against hash

---
+++ spec/phppass_spec.lua
@@ -0,0 +1,16 @@
+-- lua-phpass, Lua implementation of the portable
+-- PHP password hashing framework
+-- Copyright (C) 2015 Boris Nagaev
+-- See the LICENSE file for terms of use.
+
+describe("phpass", function()
+    it("checks phpass hash of password test12345", function()
+        local phpass = require('phpass')()
+        assert.truthy(phpass.checkPassword('test12345',
+            '$P$9IQRaTwmfeRo7ud9Fh4E2PdI0S3r.L0'))
+        assert.falsy(phpass.checkPassword('test12346',
+            '$P$9IQRaTwmfeRo7ud9Fh4E2PdI0S3r.L0'))
+        assert.falsy(phpass.checkPassword('test12345',
+            '$P$9IQRaTwmfeRo7ud9Fh4E2PdI0S3r.L1'))
+    end)
+end)
---
+++ src/phpass.lua
@@ -0,0 +1,90 @@
+-- lua-phpass, Lua implementation of the portable
+-- PHP password hashing framework
+-- Copyright (C) 2015 Boris Nagaev
+-- See the LICENSE file for terms of use.
+
+-- Encoding. Not base64!
+
+local itoa64_ = './0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ' ..
+    'abcdefghijklmnopqrstuvwxyz'
+
+local function itoa64(index)
+    return itoa64_:sub(index + 1, index + 1)
+end
+
+local function unItoa64(char)
+    return itoa64_:find(char) - 1
+end
+
+local function encode64(data)
+    local outp = ''
+    local cur = 1
+    while cur <= #data do
+        local value = data:byte(cur)
+        outp = outp .. itoa64(value % 64)
+        cur = cur + 1
+        if cur <= #data then
+            value = value + data:byte(cur) * 256
+        end
+        outp = outp .. itoa64(math.floor(value / 2^6) % 2^6)
+        if cur > #data then
+            break
+        end
+        cur = cur + 1
+        if cur <= #data then
+            value = value + data:byte(cur) * 256^2
+        end
+        outp = outp .. itoa64(math.floor(value / 2^12) % 2^6)
+        if cur > #data then
+            break
+        end
+        cur = cur + 1
+        outp = outp .. itoa64(math.floor(value / 2^18) % 2^6)
+    end
+    return outp
+end
+
+local function startsWith(str, prefix)
+   return str:sub(1, prefix:len()) == prefix
+end
+
+return function()
+    local phpass = {}
+
+    local function md5(data)
+        local md5object = require("crypto").digest.new('md5')
+        local raw = true
+        return md5object:final(data, raw)
+    end
+
+    function phpass.cryptPrivate(pw, setting)
+        local outp = startsWith(setting, '*0')
+            and '*1' or '*0'
+        if not startsWith(setting, '$P$') and
+                not startsWith(setting, '$H$') then
+            return outp
+        end
+        local count_code = setting:sub(4, 4)
+        local count_log2 = unItoa64(count_code)
+        if count_log2 < 7 or count_log2 > 30 then
+            return outp
+        end
+        local count = 2 ^ count_log2
+        local salt = setting:sub(5, 12)
+        if #salt ~= 8 then
+            return outp
+        end
+        assert(type(pw) == 'string')
+        local hx = md5(salt .. pw)
+        for i = 1, count do
+            hx = md5(hx .. pw)
+        end
+        return setting:sub(1, 12) .. encode64(hx)
+    end
+
+    function phpass.checkPassword(pw, stored_hash)
+        local hx = phpass.cryptPrivate(pw, stored_hash)
+        return hx == stored_hash
+    end
+    return phpass
+end

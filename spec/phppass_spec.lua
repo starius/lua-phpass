b8e7faa7a8cf6047a185a866fb38fbf2713e0d81
Boris Nagaev <bnagaev@gmail.com>
2015-05-13 03:25:20 +0300

    test: returns false for malformed hashes

--- spec/phppass_spec.lua
+++ spec/phppass_spec.lua
@@ -14,6 +14,22 @@ describe("phpass", function()
             '$P$9IQRaTwmfeRo7ud9Fh4E2PdI0S3r.L1'))
     end)
 
+    it("returns false for malformed hashes", function()
+        local phpass = require('phpass')
+        -- $Q
+        assert.falsy(phpass.checkPassword('test12345',
+            '$Q$9IQRaTwmfeRo7ud9Fh4E2PdI0S3r.L0'))
+        -- $1 (count_log2 too low)
+        assert.falsy(phpass.checkPassword('test12345',
+            '$P$1IQRaTwmfeRo7ud9Fh4E2PdI0S3r.L0'))
+        -- $a (count_log2 too high)
+        assert.falsy(phpass.checkPassword('test12345',
+            '$P$aIQRaTwmfeRo7ud9Fh4E2PdI0S3r.L0'))
+        -- short salt
+        assert.falsy(phpass.checkPassword('test12345',
+            '$P$9IQR'))
+    end)
+
     it("generates hash for password test12345", function()
         local phpass = require('phpass')
         local hash = phpass.hashPassword('test12345')
